2b260d86021c544ad81b9a9bf2426fb619141521
mol1986@naver.com <mol1986@naver.com>
2022-02-18 15:53:41 +0900

    Step 5 - Text Animation

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -14,8 +14,15 @@ public class NPC : MonoBehaviour, IInteraction
 
     public void Interaction()
     {
-        Debug.Log("상점 구매창 열기");
-        ShopManager.Instance.OpenShop();
+        //Debug.Log("상점 구매창 열기");
+        //ShopManager.Instance.OpenShop();
+
+        TalkManager.Instance.Talk(new string[] { 
+            "안녕?",
+            "이곳은 우리가 게임을 개발하는 곳이야.",
+            "수업이 끝나버렸다.",
+            "주말 잘 쉬다 오자."
+        });
     }
 
     public string GetName()
